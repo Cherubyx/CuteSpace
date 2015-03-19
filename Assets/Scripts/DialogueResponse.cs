16d01a9b9a855699457f7a2d28ff111cb1ea9090
louismclean <louis.mclean@gmail.com>
2015-03-19 16:16:10 -0400

    Stubbed DialogueNode and DialogueResponse

---
+++ Assets/Scripts/DialogueResponse.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueResponse {
+	string responseText;
+	DialogueNode nextNode;
+	//Conditions for response?
+	//Triggers and flags?
+}
