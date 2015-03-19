e38c8d68f10a34d6b77d06c9d97b578d08f0b9ce
Tiffany Ip <tiffany.ip18@gmail.com>
2015-03-19 14:28:25 -0400

    Added save load game script and art for main menu WIP.

---
+++ Assets/Scripts/Managers/GameData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class GameData : MonoBehaviour {
+
+    public static GameData current; //game save state
+
+    //Other classes or objects you want to persist between scenes can be here too.
+
+}
