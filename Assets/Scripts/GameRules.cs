0b87b33587c01e42d66d7edb4fe67bf919ebc2b9
louismclean <louis.mclean@gmail.com>
2015-03-30 20:08:35 -0400

    Added PrefabDictionary

--- Assets/Scripts/GameRules.cs
+++ Assets/Scripts/GameRules.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class GameRules : MonoBehaviour {
 
+	PrefabDictionary prefabDictionary;
+	float spawnTimer;
+
 	// Use this for initialization
 	void Start () {
+		spawnTimer = 5.0f;
+		prefabDictionary = GameObject.Find ("PrefabDictionary").GetComponent<PrefabDictionary>();
 		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		spawnTimer -= Time.deltaTime;
+		if (spawnTimer <= 0f) {
+			prefabDictionary.instantiatePrefab ("pirate", new Vector3 (0f, 0f, 0f), Quaternion.identity);
+			spawnTimer = 5.0f;
+		}
+
 	
 	}
 }
---
+++ Assets/Scripts/PrefabDictionary.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PrefabDictionary : MonoBehaviour {
+
+	Dictionary<string, GameObject> prefabDictionary;
+
+	public GameObject ship_300i;
+	public GameObject ship_pirate;
+	public GameObject ship_broadsword;
+
+	// Use this for initialization
+	void Start () {
+		prefabDictionary = new Dictionary<string, GameObject> ();
+		prefabDictionary.Add ("300i", ship_300i);
+		prefabDictionary.Add ("pirate", ship_pirate);
+		prefabDictionary.Add ("broadsword", ship_broadsword);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public GameObject getPrefab(string prefabName){
+		return prefabDictionary[prefabName];
+	}
+
+	public GameObject instantiatePrefab(string prefabName, Vector3 position, Quaternion rotation){
+		return Instantiate (prefabDictionary [prefabName], position, rotation) as GameObject;
+	}
+}
