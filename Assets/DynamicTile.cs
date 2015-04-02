b270b3431cdae1b434fd0942523d9beda7818b3d
Louis McLean <louis.mclean@gmail.com>
2015-04-02 00:18:34 -0400

    dynamic background tiles

--- Assets/DynamicTile.cs
+++ Assets/DynamicTile.cs
@@ -6,8 +6,8 @@ public class DynamicTile : MonoBehaviour {
 	//16 tiles of 2.25 x
 	//8 tiles of 1.19 y
 
-	float x_cameraDistanceThreshold = 32.0f;
-	float y_cameraDistanceThreshold = 12.0f;
+	float x_cameraDistanceThreshold = 30.0f;
+	float y_cameraDistanceThreshold = 30.0f;
 
 	Camera camera;
 	// Use this for initialization
@@ -19,17 +19,17 @@ public class DynamicTile : MonoBehaviour {
 	void Update () {
 		Vector3 pos = this.transform.position;
 		if(camera.gameObject.transform.position.x - this.transform.position.x > x_cameraDistanceThreshold){
-			pos.x = pos.x + 28*2.25f;
+			pos.x = pos.x + 60f;
 		}
 		if(this.transform.position.x - camera.gameObject.transform.position.x > x_cameraDistanceThreshold){
-			pos.x = pos.x - 28*2.25f;
+			pos.x = pos.x - 60f;
 		}
 
 		if(camera.gameObject.transform.position.y - this.transform.position.y > y_cameraDistanceThreshold){
-			pos.y = pos.y + 20*1.19f;
+			pos.y = pos.y + 60f;
 		}
 		if(this.transform.position.y - camera.gameObject.transform.position.y > y_cameraDistanceThreshold){
-			pos.y = pos.y - 20*1.19f;
+			pos.y = pos.y - 60f;
 		}
 
 		this.transform.position = pos;
