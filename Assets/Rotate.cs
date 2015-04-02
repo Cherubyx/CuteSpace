b7a3f4096b0fa7dc5600ded8276455008b650b73
Louis McLean <louis.mclean@gmail.com>
2015-04-02 13:36:57 -0400

    CuteSpace corgi mascot now spins a bit

---
+++ Assets/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float rotationRate = 180f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(0f,0f,Time.deltaTime * rotationRate);
+	}
+}
