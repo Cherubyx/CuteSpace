24296b71cd96dcc98601bb14735863eaf15796a4
Michael Lavoie <lavoie6453@gmail.com>
2015-04-11 09:29:45 -0400

    Partially implemented AI_LaserGuy (still need to implement wander). The behavior of the ship is as follows: * As long as there is enough energy, stay close to the target * Seek the target if it is too far * Move towards the target if it is in our FOV (This causes a flanking effect) * If there is not much energy left, we flee and try to keep our target in our rear FOV

--- Assets/Scripts/AI/AI_LaserGuy.cs
+++ Assets/Scripts/AI/AI_LaserGuy.cs
@@ -3,192 +3,159 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AI_LaserGuy : MonoBehaviour {
+    public Transform target;
 
-	delegate void AI_behaviour();
-	AI_behaviour delegatedBehaviour;
-	
-	public Vector2 orientationTarget;
-	public GameObject targetGameObject;
-	public Stack<GameObject> targetStack;
-	
-	//Wander parameters
-	private float wander_closingDistance = 1.0f;
-	private float wander_forwardThrustTargetAngle = 45f;
-	
-	//Flee parameters
-	private float flee_forwardThrustTargetAngle = 30f;
-	
-	//Attack parameters
-	private float attack_minimumDistance = 2.0f;
-	private float attack_firingDistance = 4.0f;
-	private float attack_forwardThrustTargetAngle = 15f;
-	private float attack_firingArc = 10f;
+    // If we are within this distance to the target, we should attack
+    [SerializeField]
+    private float attackingDistance = 4.0f;
 
-	//TODO: Replace strafe with some cool new behaviour
-	//Strafe parameters
-	private float strafe_maximumDistance = 8.0f;
-	private float strafe_avoidanceArc = 90f;
-	private float strafe_minimumVelocity = 2.0f;
-	
-	
-	private ShipControl shipControl;
-	
-	// Use this for initialization
-	void Start () {
-		targetStack = new Stack<GameObject>();
+    // Field of view (in degrees)
+    [SerializeField]
+    private float fov = 50.0f;
 
-		//TODO: remove test code
-		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Team2");
-		foreach(GameObject target in enemies){
-			targetStack.Push (target);
-		}
+    // Rear field of view (For fleeing; To know when the target is behind us)
+    [SerializeField]
+    private float rearFov = 10.0f;
 
-		delegatedBehaviour = attack;
-		orientationTarget = new Vector2(0f,0f);
-		shipControl = this.gameObject.GetComponent<ShipControl>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//If we don't have a target, check the target stack!
-		if(targetGameObject == null && targetStack.Peek() != null){
-			targetGameObject = targetStack.Pop();
-		}
-		
-		delegatedBehaviour();
-	}
-	
-	//Wander Behaviour
-	private void wander(){
-		//State transitions
-		//If we have a target, attack it
-		if( targetGameObject != null ){
-			delegatedBehaviour = attack;
-		}
-		wander_updateRotation();
-		wander_updateThrust();
-	}
-	
-	void wander_updateRotation(){
-		orientationTarget = orientationTarget + new Vector2(Random.Range(-5f,5f) * Time.deltaTime,Random.Range(-5f,5f) * Time.deltaTime);
-		shipControl.updateRotation(orientationTarget);
-	}
-	
-	void wander_updateThrust(){
-		if(Vector2.Distance (this.transform.position, orientationTarget) > wander_closingDistance && Mathf.Abs (MathHelper.getRemainingAngleToTarget(this.gameObject,orientationTarget)) < wander_forwardThrustTargetAngle) {
-			shipControl.activateMainEngines ();
-		}
-		else {
-			shipControl.cutMainEngines();
-		}	
-	}
-	
-	//Flee Behaviour
-	private void flee(){
-		//State transitions
-		//If energy is full, get back into the fight
-		if( targetGameObject == null ){
-			delegatedBehaviour = wander;
-			return;
-		}
-		else if(shipControl.energy == shipControl.maxEnergy){
-			delegatedBehaviour = attack;
-		}
-		
-		//State behaviours
-		flee_updateRotation();
-		flee_updateThrust();
-	}
-	
-	void flee_updateRotation(){
-		if(targetGameObject != null){
-			Vector2 vectorToTarget = targetGameObject.transform.position - this.transform.position;
-			orientationTarget = (Vector2)this.transform.position - vectorToTarget;
-			shipControl.updateRotation(orientationTarget);
-		}
-	}
-	
-	void flee_updateThrust(){
-		if(Mathf.Abs (MathHelper.getRemainingAngleToTarget(this.gameObject,orientationTarget)) < flee_forwardThrustTargetAngle) {
-			shipControl.activateMainEngines ();
-		}
-		else {
-			shipControl.cutMainEngines();
-		}	
-	}
-	
-	//Attack Behaviour
-	private void attack(){
-		//State transitions
-		//If energy is low (below 10%), flee
-		if( targetGameObject == null ){
-			delegatedBehaviour = wander;
-			shipControl.ceaseFirePrimaryWeapons();
-			return;
-		}
-		else if(shipControl.energy < shipControl.maxEnergy / 10f){
-			delegatedBehaviour = flee;
-			shipControl.ceaseFirePrimaryWeapons();
-		}
-		else if(Vector2.Distance (this.transform.position, targetGameObject.transform.position) < attack_minimumDistance){
-			delegatedBehaviour = strafe;
-		}
-		
-		//State behaviours
-		attack_updateRotation();
-		attack_updateThrust();
-		attack_updateFire();
-		
-	}
-	
-	void attack_updateRotation(){
-		if(targetGameObject != null){
-			orientationTarget = targetGameObject.transform.position;
-			shipControl.updateRotation(orientationTarget);
-		}
-	}
-	
-	void attack_updateThrust(){
-		if(targetGameObject != null && Vector2.Distance (this.transform.position, targetGameObject.transform.position) > attack_firingDistance && Mathf.Abs (MathHelper.getRemainingAngleToTarget(this.gameObject,targetGameObject.transform.position)) < attack_forwardThrustTargetAngle) {
-			shipControl.activateMainEngines ();
-		}
-		else {
-			shipControl.cutMainEngines();
-		}	
-	}
-	
-	void attack_updateFire(){
-		if(targetGameObject != null && Vector2.Distance(this.transform.position,targetGameObject.transform.position) < attack_firingDistance && Mathf.Abs (MathHelper.getRemainingAngleToTarget(this.gameObject,targetGameObject.transform.position)) < attack_firingArc){
-			shipControl.firePrimaryWeapons();
-		}
-		else{
-			shipControl.ceaseFirePrimaryWeapons();
-		}
-	}
+    public Vector2 DirectionToTarget { get; set; }
+    public float DistanceToTarget { get; set; }
 
-	//TODO: Replace strafe with some appropriate laser-y behaviour
-	//Strafe behaviour	
-	private void strafe(){
-		//State transitions
-		if( targetGameObject == null ){
-			delegatedBehaviour = wander;
-			return;
-		}
-		else if(Vector2.Distance (this.transform.position, targetGameObject.transform.position) > strafe_maximumDistance){
-			delegatedBehaviour = attack;
-		}
-		
-		//State behaviours
-		Vector2 vectorToTarget = targetGameObject.transform.position - this.transform.position;
-		Vector2 velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;
-		
-		if(Mathf.Abs(Vector2.Angle(vectorToTarget,velocity)) < strafe_avoidanceArc || velocity.magnitude < strafe_minimumVelocity){
-			flee_updateRotation();
-			flee_updateThrust();
-		}
-		else{
-			attack_updateRotation();
-			attack_updateFire();
-		}
-	}
+    private Stack<Transform> targets;
+
+    private delegate void AI_behaviour();
+    private AI_behaviour delegatedBehaviour;
+
+    private ShipControl shipControl;
+
+    // Use this for initialization
+    protected void Start() {
+        targets = new Stack<Transform>();
+
+        //TODO: remove test code
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Team2");
+        foreach (GameObject e in enemies) {
+            targets.Push(e.transform);
+        }
+
+        target = targets.Pop();
+
+        delegatedBehaviour = Seek;
+        shipControl = gameObject.GetComponent<ShipControl>();
+    }
+
+    protected void Update() {
+        if (target == null) {
+            // If there are no targets to attack, then we wander
+            if (targets.Count > 0 ) {
+                target = targets.Pop();
+            } else {
+                delegatedBehaviour = Wander;
+            }
+        } else {
+            // Calculate commonly required properties
+            DirectionToTarget = target.position - transform.position;
+            DistanceToTarget = DirectionToTarget.magnitude;
+            DirectionToTarget.Normalize();
+        }
+
+        delegatedBehaviour();
+    }
+
+    private void Wander() {
+    }
+
+    private void Flee() {
+        // Seek the target once we are fully recharged
+        if (IsEnergyRecharged()) {
+            delegatedBehaviour = Seek;
+        } else {
+            // Move forwards if the target is behind us
+            if (IsTargetWithinRearFov()) {
+                MoveForward();
+            } else {
+                StopMovingForward();
+
+                // Rotate away from the target
+                RotateTowards((Vector2)transform.position - DirectionToTarget);
+            }
+        }
+    }
+
+    private void Attack() {
+        // Flee is our energy is too low
+        if (IsEnergyLow()) {
+            CeaseFirePrimaryWeapon();
+            delegatedBehaviour = Flee;
+        } else {
+            // Check if we have drifted too far from the target
+            if (IsTargetWithinAttackingDistance()) {
+                if (IsTargetWithinFov()) {
+                    FirePrimaryWeapon();
+                } else {
+                    CeaseFirePrimaryWeapon();
+                    RotateTowards(target.position);
+                }
+            } else {
+                CeaseFirePrimaryWeapon();
+                delegatedBehaviour = Seek;
+            }
+        }
+    }
+
+    private void Seek() {
+        // Attack if we are close to the target
+        if (IsTargetWithinAttackingDistance()) {
+            delegatedBehaviour = Attack;
+        } else {
+            // Move towards target if it is within our fov
+            if (IsTargetWithinFov()) {
+                MoveForward();
+            } else {
+                StopMovingForward();
+
+                // Rotate towards the target
+                RotateTowards(target.position);
+            }
+        }
+    }
+
+    private void RotateTowards(Vector2 position) {
+        shipControl.updateRotation(position);
+    }
+
+    private void MoveForward() {
+        shipControl.activateMainEngines();
+    }
+
+    private void StopMovingForward() {
+        shipControl.cutMainEngines();
+    }
+
+    private void FirePrimaryWeapon() {
+        shipControl.firePrimaryWeapons();
+    }
+
+    private void CeaseFirePrimaryWeapon() {
+        shipControl.ceaseFirePrimaryWeapons();
+    }
+
+    private bool IsTargetWithinAttackingDistance() {
+        return DistanceToTarget <= attackingDistance;
+    }
+
+    private bool IsTargetWithinFov() {
+        return Vector2.Angle(transform.up, DirectionToTarget) <= (fov / 2.0f);
+    }
+
+    private bool IsTargetWithinRearFov() {
+        return Vector2.Angle(-transform.up, DirectionToTarget) <= (rearFov / 2.0f);
+    }
+
+    private bool IsEnergyLow() {
+        return shipControl.energy < (shipControl.maxEnergy / 10.0f);
+    }
+
+    private bool IsEnergyRecharged() {
+        return shipControl.energy >= shipControl.maxEnergy;
+    }
 }
