af007e49e96f6ba51555b33ca5452b0cc7d59c79
Michael Lavoie <lavoie6453@gmail.com>
2015-04-11 17:13:22 -0400

    Implemented basic AI fleeting for the Heron ship. The AI_Fleet script contains a collection of fleet members that are assigned targets to seek (the slot positions). The AI_Fleet has its own target that it seeks and the slot positions follow (because they are children of the Fleet gameobject). In the 'BlueLaserGuy vs Pirates' scene, an example fleet is set up to move towards the Crab Dragon.
    Known issues:
    * If ships of the fleet collide, they sometimes get stuck on eachother (really difficult to fix without allowing the ships to thrust backward).

---
+++ Assets/Scripts/AI/AI_Fleet.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Manages a fleet. The transform of this
+/// class is used to represent the 'anchor' point of
+/// the fleet. The target represents where the anchor point
+/// and its associated slots are moving to. The anchor point
+/// will attempt to reach the target and the slots will stay
+/// relative to the anchor.
+/// 
+/// Important: At the moment, this works only with AI
+/// having the AI_Heron script attached.
+/// </summary>
+public class AI_Fleet : MonoBehaviour {
+    public Transform target;
+
+    // This speed should depend on the speed of the member
+    // ships. If the target moves to slowly, the behaviour
+    // of the ships be look odd because they will be transitioning
+    // between arrive and seek behaviours.
+    public float targetSpeed = 1.0f;
+
+    [System.Serializable]
+    struct FleetMember {
+        public Transform member;
+        public Transform slot;
+    }
+
+    [SerializeField]
+    private List<FleetMember> members;
+
+    // These values are updated once every frame
+    private Vector2 directionToTarget;
+    private float distanceToTarget;
+
+    private delegate void AI_behaviour();
+    private AI_behaviour delegatedBehaviour;
+
+    protected void Start() {
+        delegatedBehaviour = Seek;
+
+        // Assign members to their respective slots
+        foreach (FleetMember m in members) {
+            m.member.GetComponent<AI_Heron>().target = m.slot;
+        }
+    }
+
+    protected void Update() {
+        if (target != null) {
+            // Calculate commonly required properties
+            directionToTarget = target.position - transform.position;
+            distanceToTarget = directionToTarget.magnitude;
+            directionToTarget.Normalize();
+
+            delegatedBehaviour();
+        }
+    }
+
+    private void Seek() {
+        transform.position = (Vector2)transform.position + targetSpeed * directionToTarget * Time.deltaTime;
+        transform.up = directionToTarget;
+    }
+
+    protected void OnDrawGizmos() {
+        foreach (FleetMember m in members) {
+            Gizmos.DrawWireSphere(m.slot.position, 0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/AI/AI_Heron.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ShipControl))]
+public class AI_Heron : MonoBehaviour {
+    public Transform target;
+
+    // We will be satisfied when this radius is reached
+    [SerializeField]
+    private float targetRadius = 1.0f;
+
+    // Field of view (in degrees)
+    [SerializeField]
+    private float fov = 50.0f;
+
+    // These values are updated once every frame
+    private Vector2 directionToTarget;
+    private float distanceToTarget;
+
+    private delegate void AI_behaviour();
+    private AI_behaviour delegatedBehaviour;
+
+    private ShipControl shipControl;
+
+    // Use this for initialization
+    protected void Start() {
+        delegatedBehaviour = Seek;
+        shipControl = gameObject.GetComponent<ShipControl>();
+    }
+
+    protected void Update() {
+        if (target != null) {
+            // Calculate commonly required properties
+            directionToTarget = target.position - transform.position;
+            distanceToTarget = directionToTarget.magnitude;
+            directionToTarget.Normalize();
+
+            delegatedBehaviour();
+        }
+    }
+
+    private void Seek() {
+        if (IsWithinTargetRadius()) {
+            delegatedBehaviour = Align;
+            StopMovingForward();
+            return;
+        }
+
+        // Move towards target if it is within our fov
+        if (IsTargetWithinFov()) {
+            MoveForward();
+        } else {
+            StopMovingForward();
+
+            // Rotate towards the target
+            RotateTowards(target.position);
+        }
+    }
+
+    private void Align() {
+        if (!IsWithinTargetRadius()) {
+            delegatedBehaviour = Seek;
+        } else {
+            // If we are here, then we are in our slot position
+            // and we need to align to the slot's orientation
+            RotateTowards(transform.position + target.up);
+        }
+    }
+
+    private void RotateTowards(Vector2 position) {
+        shipControl.updateRotation(position);
+    }
+
+    private void MoveForward() {
+        shipControl.activateMainEngines();
+        shipControl.cutSpaceBrake();
+    }
+
+    private void StopMovingForward() {
+        shipControl.cutMainEngines();
+        shipControl.activateSpaceBrake();
+    }
+
+    private bool IsTargetWithinFov() {
+        return Vector2.Angle(transform.up, directionToTarget) <= (fov / 2.0f);
+    }
+
+    private bool IsWithinTargetRadius() {
+        return distanceToTarget < targetRadius;
+    }
+}
