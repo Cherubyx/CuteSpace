322948f90ee11763e2afa0a026a591383bdadd1c
Louis McLean <louis.mclean@gmail.com>
2015-04-12 15:38:44 -0400

    Added music manager and jump gate indicator (lets you know where the jump gates are!)

--- Assets/Scripts/JumpGate.cs
+++ Assets/Scripts/JumpGate.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class JumpGate : MonoBehaviour {
 
 	public string exitSystemName;
+	public Texture2D pointerArrow;
 
 	// Use this for initialization
 	void Start () {
@@ -25,5 +26,19 @@ public class JumpGate : MonoBehaviour {
 
 	IEnumerator WaitFor(float waitTime) {
 		yield return new WaitForSeconds(waitTime);
+	}
+
+	void OnGUI()
+	{
+		if(Camera.current != null){
+			Vector2 bottomLeft = Camera.current.ScreenToWorldPoint(Vector2.zero);
+			Vector2 topRight = Camera.current.ScreenToWorldPoint(new Vector2(Camera.current.pixelWidth, Camera.current.pixelHeight));
+			Vector2 pos = transform.position;
+			pos.x = Mathf.Clamp(pos.x -1f, bottomLeft.x, topRight.x);
+			pos.y = Mathf.Clamp(pos.y -1f, bottomLeft.y, topRight.y);
+			pos = Camera.current.WorldToScreenPoint(pos);
+			//GUI.DrawTexture(new Rect(pos.x, Screen.height - pos.y, 28f, 42f), pointerArrow);
+			GUI.TextArea(new Rect(pos.x, Screen.height - pos.y, 150f,50f),"Jumpgate to "+exitSystemName);
+		}
 	}
 }
---
+++ Assets/Scripts/Pulsate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulsate : MonoBehaviour {
+
+	public Color color1;
+	public Color color2;
+	public float maxSizeFactor = 1.2f;
+	public float colorCycleOffset;
+	Vector3 startScale;
+
+	// Use this for initialization
+	void Start () {
+		startScale = this.transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<SpriteRenderer>().color = Color.Lerp(color1,color2,MathHelper.functionA(Time.time + colorCycleOffset));		
+		float scaleScalar = Mathf.Lerp(maxSizeFactor,1.0f,MathHelper.functionA(Time.time + colorCycleOffset));
+		this.transform.localScale = startScale * scaleScalar;	
+	}
+}
