31c8ba401e5c875ea1067a4fbc1744d715cded5e
Louis McLean <louis.mclean@gmail.com>
2015-04-13 00:18:36 -0400

    wip overworld encounter

---
+++ Assets/OverworldNPCAvatar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverworldNPCAvatar : MonoBehaviour {
+
+	public GameObject playerAvatar;
+	private string npcName;
+
+	// Use this for initialization
+	void Start () {
+		npcName = "default";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 dir = playerAvatar.transform.position - transform.position;
+		float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+		angle -= 90f;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		transform.position = Vector2.MoveTowards(transform.position,playerAvatar.transform.position,1.0f * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject == playerAvatar){
+
+		}
+	}
+}
