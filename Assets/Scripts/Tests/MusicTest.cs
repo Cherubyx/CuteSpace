33a49f029a5f3ea222aa8a0fa99f62a2eac3ff4e
Michael Lavoie <lavoie6453@gmail.com>
2015-04-13 16:44:44 -0400

    Implemented the MusicManager.

---
+++ Assets/Scripts/Tests/MusicTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicTest : MonoBehaviour {
+
+	void Start () {
+        MusicManager.Instance.Play("Main");
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            MusicManager.Instance.Play("Pew");
+        }
+	}
+}
